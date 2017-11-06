48a93b91f2595a3d1695bfd6f3cd042d768cd247
sun474473556 <474473556@qq.com>
2017-11-06 23:06:57 +0800

    mysite

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mysite project.
+WSGI config for a project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -13,5 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-
 application = get_wsgi_application()
+
+
